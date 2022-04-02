778fe947dcee07cc93f5daaa588b936e8fcea80d
sunset wan <sunsetwan@gmail.com>
2022-04-02 13:55:20 +0800

    Initial commit

---
+++ Example/CustomRouter/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CustomRouter
+//
+//  Created by sunset wan on 04/02/2022.
+//  Copyright (c) 2022 sunset wan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
